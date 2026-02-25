e1bfd886044c9f4390ac87f1b87d72a893fe3404
Michael <michaelgss009@gmail.com>
2026-02-25 10:40:01 +0100

    Added Piezo buzzer to code

--- sketch.ino
+++ sketch.ino
@@ -1,13 +1,19 @@
-int apin = 34;
+int ppin = 21; // Piezo pin
+int apin = 34; // Analog pin for Poty
 int val;
 
 void setup()
 {
     Serial.begin(9600);
+    pinMode(21, OUTPUT);
 }
 
 void loop()
 {
+
     val = analogRead(apin);
+
+    tone(21, val);
+
     Serial.println(val);
 }
